59112ae812fbbd13a8d2c4be65827f37d17d2c0f
jonnyhuck <jonnyhuck@gmail.com>
2022-04-20 14:10:34 +0100

    initial commit

---
+++ sc2shp.py
@@ -0,0 +1,129 @@
+"""
+Simple script to parse GPS data from Storage Connect into a GeoDataFrame and export to OGC GeoPackage (Shapefiles can't handle datetime fields)
+@author jonnyhuck
+
+Data Source:
+    https://storageconnect.its.manchester.ac.uk/Data/Export/579ad18d-4732-402d-822f-38da12e9edc5
+
+Example Usage:
+    python sc2shp.py -i "/Users/jonnyhuck/Downloads/Export2022-04-19T16-23-09.json" -o "/Users/jonnyhuck/Downloads/gps_traces.gpkg"
+"""
+
+from json import load as load_json
+from shapely.geometry import Point
+from geopandas import GeoDataFrame
+from argparse import ArgumentParser
+from datetime import datetime as dt
+from fiona.errors import DriverIOError
+from pandas import DataFrame, concat, to_datetime
+
+def parse_data(in_path, out_path="", export=True, report=True, debug=False):
+    """
+    Parse json data and export to Shapefile
+
+    @param in_path:     (str) file path for the *.json file to be parsed
+    @param out_path:    (str) file path for the output *.gpkg file (only used if `export=True`) 
+    @param export:      (boolean) indicating whether a *.gpkg file should be exported
+    @param report:      (boolean) indicating whether to report the number of points per user
+    @param debug:       (boolean) indicating whether to output data packets that failed to import to the console
+    """
+    # ensure input is a JSON file
+    if in_path[-5:] != ".json":
+        print("ERROR: Input file must be *.json file")
+        exit(1)
+    
+    # ensure output is shapefile
+    if export:
+        if out_path[-5:] != ".gpkg":
+            print("ERROR: Output file path must be *.gpkg if --export=True")
+            exit(1)
+
+    # load and parse json dataset
+    try:
+        with open(in_path) as json_file:
+            data = load_json(json_file)
+    except FileNotFoundError:
+        print("Input file not found")
+        exit(1)
+
+    # loop therough each data packet
+    dfs = []
+    for d in data['packets']:
+
+        # construct dataframe and load into list
+        try:
+            dfs.append(DataFrame({
+                'user_id':      [d['user_id']]*len(d['longitude']),
+                'longitude':    [float(n.replace(",",".")) for n in d['longitude']],
+                'latitude':     [float(n.replace(",",".")) for n in d['latitude']],
+                'timestamp':    d['timestamp'], #[dt.strptime(n, "%b %d %H:%M:%S %Y %Z") for n in d['timestamp']],
+                'accuracy':     [int(n) for n in d['accuracy']],
+            }))
+            # dfs[-1].timestamp = to_datetime(dfs[-1].timestamp, nfer_datetime_format=True)
+        except ValueError as e:
+            if debug:
+                print(e)
+                print(d['user_id'])
+                print(d['longitude'])
+                print(d['latitude'])
+                print(d['timestamp'])
+                print(d['accuracy'])
+                print()
+            continue
+
+    # concat all DataFrames into a single one
+    df = concat(dfs)
+
+    # parse date and time
+    df['timestamp'] = to_datetime(df['timestamp'], infer_datetime_format=True)
+
+    # convert to geodataframe
+    gdf = GeoDataFrame(
+        df.drop(['longitude', 'latitude'], axis=1),
+        crs='+proj=longlat +datum=WGS84 +no_defs',
+        geometry=[Point(ll) for ll in zip(df.longitude, df.latitude)])
+    del df
+
+    # print count per users
+    if report:
+        print()
+        print(gdf['timestamp'].groupby(gdf.user_id).agg(First_Log='min', Last_Log='max', N_Logs='count').reset_index().sort_values(['N_Logs'], ascending=False))
+        print()
+
+    # export to shapefile
+    if export:
+        try:
+            schema = {
+                'geometry': 'Point',
+                'properties': {
+                    'user_id': 'str',
+                    'timestamp': 'datetime',
+                    'accuracy': 'int',
+                }
+            }
+            gdf.to_file(out_path, driver='GPKG', layer='gps_traces', schema=schema)
+        except DriverIOError as e:
+            print(e)
+            print("ERROR: Output file path invalid")
+    del gdf
+
+# parse arguments and pass to function
+if __name__ == '__main__':
+
+    # dt.strptime("2022-04-16 10:50:46 GMT-04:00", "%Y-%m-%d $H:%M:%S GMT%z")
+    # exit()
+    parser = ArgumentParser(description='Simple script to parse GPS data from Storage Connect into a GeoDataFrame and export to Shapefile.')
+    parser.add_argument('-i', '--in_path', help='File Path for input file (*.json)', required=True)
+    parser.add_argument('-o', '--out_path', help='File path for output file (*.gpkg)', required=False)
+    parser.add_argument('-e', '--export', help='Export data to shalefile (True/False)', required=False)
+    parser.add_argument('-r', '--report', help='Report number of per user (True/False)', required=False)
+    parser.add_argument('-d', '--debug', help='Print out data that fail to parse (True/False)', required=False)
+    args = vars(parser.parse_args())
+    kwargs = {}
+    for arg in args:
+        if args[arg]:
+            if args[arg].lower() in ['true', 'false']:
+                kwargs[arg] = args[arg].lower() == 'true'
+            else:
+                kwargs[arg] = args[arg]
+    parse_data(**kwargs)
